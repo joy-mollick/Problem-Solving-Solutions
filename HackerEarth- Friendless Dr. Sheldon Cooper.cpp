866f4a813279b2471d5b14c386d337583521fd3f
Nayeem Mollick Joy <nayeemjoy59@gmail.com>
2019-07-17 02:30:02 -0700

    Add files via upload

---
+++ HackerEarth- Friendless Dr. Sheldon Cooper.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+
+{
+    int tc,n,m,a,b;
+    cin>>tc;
+    while(tc--)
+    {
+        cin>>n>>m;
+        while(m--)
+        {
+            cin>>a>>b;
+        }
+        cout<<(n-1)<<endl;
+    }
+    return 0;
+}
