feb0f9cc06a7c5138e91671d5a1e08fc6e918f71
Nayeem Mollick Joy <nayeemjoy59@gmail.com>
2019-07-21 04:25:59 -0700

    Update HackerEarth-Monk and Graph G.cpp

--- HackerEarth-Monk and Graph G.cpp
+++ HackerEarth-Monk and Graph G.cpp
@@ -27,7 +27,7 @@ void bfs(int source,int *dis)
        vector<int>::iterator it;
        for(it=adj[u].begin();it!=adj[u].end();it++)
        {
-           if(dis[(*it)]>dis[(u)]+1)
+           if(dis[(*it)]>dis[(u)]+1) // minimum level finding ,that's why we are not using any visited array 
            {
                dis[(*it)]=dis[(u)]+1;
                q.push((*it));
@@ -44,15 +44,15 @@ int main()
     while(m--)
     {
         cin>>u>>v;
-        adj[u].push_back(v);
+        adj[u].push_back(v); // bidirectional graph
         adj[v].push_back(u);
     }
     cin>>s>>d;
-    int source_to_dest[n+1];
+    int source_to_dest[n+1]; 
     int dest_to_source[n+1];
 
-    bfs(s,source_to_dest);
-    bfs(d,dest_to_source);
+    bfs(s,source_to_dest);// bfs for assuming source as a source 
+    bfs(d,dest_to_source);// bfs for assuming destination as a source 
 
     vector<int>level[n];
     for(int i=1;i<=n;i++)
@@ -61,7 +61,7 @@ int main()
         {
             if(source_to_dest[i]+dest_to_source[i]==source_to_dest[d])
             {
-                level[source_to_dest[i]].push_back(i);
+                level[source_to_dest[i]].push_back(i);// That means this node is in the shortest path way 
             }
         }
     }
@@ -69,13 +69,13 @@ int main()
     int ans=INT_MAX;
     for(int i=0;i<=n;i++)
     {
-        if(level[i].size() == 1)
+        if(level[i].size() == 1) // when there is no other vertex in this level as a shortest path vertex then cutting this the path will be increased
         {
             ans=min(ans,level[i][0]);
         }
     }
 
-    if(ans==INT_MAX)
+    if(ans==INT_MAX) // there is many shortest path ,then by cutting one vertex doesn't affect the shortest path 
     cout<<"-1"<<endl;
 
     else
