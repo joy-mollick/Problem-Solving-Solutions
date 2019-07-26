661f1d602f200b05f1147ee5af00a874ead78ac0
Nayeem Mollick Joy <nayeemjoy59@gmail.com>
2019-07-26 13:35:55 -0700

    Add files via upload

---
+++ Codeforces-437C - The Child and Toy.cpp
@@ -0,0 +1,47 @@
+// Just Use Djkstra algo for two times
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+const int mx=1e3+1;
+
+typedef pair<int,int>pii;
+vector<int>adj[mx];
+int cost[mx];
+vector<pii>pq;
+int ans=0;
+
+int main()
+
+{
+    int n,m,a,b,c;
+    cin>>n>>m;
+    for(int i=1;i<=n;i++)
+    {
+        cin>>c;
+        cost[i]=c;
+        pq.push_back(make_pair(c,i));
+    }
+    while(m--)
+    {
+        cin>>a>>b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+    sort(pq.rbegin(),pq.rend());
+    int count=0;
+    for(int i=0;i<pq.size();i++)
+    {
+        count=0;
+        int u=pq[i].second;
+        for(int j=0;j<adj[u].size();j++)
+        {
+            count=count+cost[adj[u][j]];
+        }
+        cost[u]=0;
+        //cout<<u<<" "<<count<<endl;
+        ans=ans+count;
+    }
+    cout<<ans<<endl;
+}
