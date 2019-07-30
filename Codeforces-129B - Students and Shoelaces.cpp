d5203402586e3007b39faccffbae3ec32b2db993
Nayeem Mollick Joy <nayeemjoy59@gmail.com>
2019-07-30 13:41:06 -0700

    Add files via upload

---
+++ Codeforces-129B - Students and Shoelaces.cpp
@@ -0,0 +1,51 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main()
+
+{
+    int n,m,u,v;
+    cin>>n>>m;
+    map< int , pair<int,int> >edge;
+    map< pair<int,int> , bool> edge_vis;
+    int degrees[n+1];
+    memset(degrees,0,sizeof(degrees));
+    int i=0;
+    for(i=0;i<m;i++)
+    {
+        cin>>u>>v;
+        edge[i]=make_pair(u,v);
+        degrees[u]++;
+        degrees[v]++;
+    }
+    int total_group=0;
+    while(1)
+    {
+        vector< pair<int,int> >v;
+        bool group=false;
+        for(int i=0;i<m;i++)
+        {
+            pair<int,int>p;
+            p=edge[i];
+            if((degrees[p.first]==1||degrees[p.second]==1) &&edge_vis[p]==false)
+            {
+                v.push_back(p);
+                group=true;
+            }
+        }
+        for(int j=0;j<v.size();j++)
+        {
+            degrees[v[j].first]--;
+            degrees[v[j].second]--;
+            edge_vis[v[j]]=true;
+        }
+        if(group==false) // that means there are no group else like we want ,so our task have been done
+            break;
+        else{
+            total_group++;}
+    }
+    cout<<total_group<<endl;
+}
